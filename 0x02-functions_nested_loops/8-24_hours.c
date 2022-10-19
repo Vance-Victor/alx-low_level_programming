c43d00d7ec8de8d55881919938502ab3152db56d
Vance-Victor <viktohvance@gmail.com>
2022-10-19 14:05:02 -0700

    7-10 task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,14 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-
-	for (hour = 0; minute <= 59; minute++)
-	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
-		_putchar(':');
-		_putchar((minute / 10) + '0');
-		_putchar((minute % 10) + '0');
-		_putchar('\n');
-	}
+		for (hour = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
