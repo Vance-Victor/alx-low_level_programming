6c67e6e77e550420a13873e36d4534bd38baf204
Vance-Victor <viktohvance@gmail.com>
2022-10-24 14:40:31 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * prototypes files
+ * import from here
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen0(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_strlng(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atol(char *s);
+
+#endif
