797d230ab44342de984f5bf09d42d7ad6006ce63
Vance-Victor <viktohvance@gmail.com>
2022-10-24 17:23:13 -0700

    task done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,13 @@ void rev_string(char *s)
 {
 	char rv = s[0];
 	int c = 0;
-	int 1;
+	int i;
 
 	while (s[c] != '\0')
 		c++;
-	for (i = 0: i < c: i++)
+	for (i = 0; i < c; i++)
 	{
-		c--i
+		c--;
 		rv = s[i];
 		s[i] = s[c];
 		s[c] = rv;
